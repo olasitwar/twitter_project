fd406a2275a245850d571cd8bd441c106ab75be2
olasitwar <aleksandra.sitekwarda@gmail.com>
2023-01-11 18:19:35 +0100

    stream code established

---
+++ main.py
@@ -0,0 +1,26 @@
+from credentials import *
+from python.TweetRead import *
+# import tweepy
+# from tweepy import OAuthHandler
+
+if __name__ == '__main__':
+
+    wordList = ['hate', 'moron', 'bullshit']
+    # launch_stream()
+    # get_steamed_data(API_KEY, API_KEY_SECRET, ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    auth = OAuthHandler(API_KEY, API_KEY_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+
+    s = socket.socket()         # Create a socket object
+    host = "127.0.0.1"     # Get local machine name
+    port = 5555                 # Reserve a port for your service.
+    s.bind((host, port))        # Bind to the port
+
+    print("Listening on port: %s" % str(port))
+
+    s.listen(5)                 # Now wait for client connection.
+    c, addr = s.accept()        # Establish connection with client.
+
+    print( "Received request from: " + str( addr ) )
+
+    sendData(wordList, c , auth)
---
+++ start_stream.py
@@ -0,0 +1,39 @@
+import os
+from credentials import *
+os.environ["JAVA_HOME"] = JAVA_HOME_USER
+os.environ["SPARK_HOME"] = SPARK_HOME_USER
+os.environ["PATH"] = PATH_USER
+
+import findspark
+findspark.init('/home/lkw/spark-3.0.2-bin-hadoop3.2')
+from pyspark import SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.sql import SQLContext
+from pyspark.sql.functions import desc
+from collections import namedtuple
+
+# def launch_stream():
+
+if __name__ == '__main__':
+ sc = SparkContext()
+
+ ssc = StreamingContext(sc, 10)
+ sqlContext = SQLContext(sc)
+
+ socket_stream = ssc.socketTextStream("127.0.0.1", 5555)
+
+ lines = socket_stream.window(20)
+
+ fields = ("tag", "count")
+ Tweet = namedtuple('Tweet', fields)
+
+ (lines.flatMap(lambda text: text.split(" "))  # Splits to a list
+  .filter(lambda word: word.lower().startswith("#"))  # Checks for hashtag calls
+  .map(lambda word: (word.lower(), 1))  # Lower cases the word
+  .reduceByKey(lambda a, b: a + b)  # Reduces
+  .map(lambda rec: Tweet(rec[0], rec[1]))  # Stores in a Tweet Object
+  .foreachRDD(lambda rdd: rdd.toDF().sort(desc("count"))  # Sorts Them in a DF
+              .limit(10).registerTempTable("tweets")))  # Registers to a table.
+
+ print("START STREAMING...")
+ ssc.start()
